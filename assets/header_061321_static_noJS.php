86f8e1af4dd0f844cbd1393e093ef16ba4afa878
tynsoltys <c.n.soltys@gmail.com>
2021-06-14 15:37:14 -0400

    adding some prelim accordion patterns and fixing TAC button

---
+++ assets/header_061321_static_noJS.php
@@ -0,0 +1,180 @@
+<!-- HEADER/TOP NAV START-->
+<header class="top-header lsop-header">
+  <nav class="top-nav">
+    <div class="top-nav-left">
+      <a href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/members_home/?" class="logo">
+        <img
+          src="./app/images/logo-circle.png"
+          alt="big picture trading logo"
+      /></a>
+    </div>
+    <div class="hamburger">
+
+      <input type="checkbox" name="menuCheck" class="menuCheck" id="">
+      <i class="fas fa-bars"></i>
+      <div class="mobile-menu">
+        <ul>
+          <li>
+            <a href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/members_home/#daily-analytics">Daily Analytics</a>
+          </li>
+          <li>
+            <a href="https://members.bigpicturetrading.com/">Trades & Portfolios</a>
+          </li>
+          <li>
+            <a href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/members_home/#master-programs">Master Programs</a>
+          </li>
+          <li>
+            <a href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/members_home/#upcoming-events">Upcoming Events</a>
+          </li>
+          <li>
+            <a href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/members_home/#resources">Resources</a>
+          </li>
+          <li>
+            <a href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/members_home/#tools">Tools</a>
+          </li>
+
+          <li>
+            <a href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/members_home/#twitter">Twitter Feed</a>
+          </li>
+          <li>
+            <a href="http://secure.bigpicturetrading.com/membership/profile">Account</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="top-nav-right">
+      <ul class="top-menu top-nav-primary">
+        <li>
+          <a class="top-level-side-nav" href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/members_home/#daily-analytics">
+            <div class="side-link-icon">
+              <i class="fas fa-analytics"></i>
+            </div>
+            <span>daily analytics</span>
+          </a>
+          <ul class="top-nav-secondary">
+            <li>
+              <a href="#daily-analytics">Macro Outlook</a>
+            </li>
+            <li>
+              <a href="#daily-analytics">Where is the Trade?</a>
+            </li>
+            <li>
+            <a href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/members_home/recording_archives.php">Recording Archive</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a class="top-level-side-nav" href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/members_home/">
+            <div class="side-link-icon">
+              <i class="fas fa-bell"></i>
+            </div>
+            <span>Trades & Portfolios</span>
+          </a>
+          <ul class="top-nav-secondary">
+            <li id="menu-item-child-910">
+                <a href="https://members.bigpicturetrading.com/" class="">Recent Activity</a>
+            </li>
+            <li id="menu-item-child-813">
+                <a href="https://members.bigpicturetrading.com/bot/trade_status/active/" class="">BreakOut Trades</a>
+            </li>
+            <li id="menu-item-child-814">
+                <a href="https://members.bigpicturetrading.com/cct/trade_status/active/" class="">Commodity Cycle Trades</a>
+            </li>
+            <li id="menu-item-child-816">
+                <a href="https://members.bigpicturetrading.com/ofi/trade_status/active/" class="">Options for Income</a>
+            </li>
+            <li id="menu-item-child-817">
+                <a href="https://members.bigpicturetrading.com/lto/trade_status/active/" class="">Long Term Options</a>
+            </li>
+            <li id="menu-item-child-818">
+                <a href="https://members.bigpicturetrading.com/tactical-portfolio/" class="">Tactical Portfolio</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a class="top-level-side-nav" href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/members_home/#master-programs">
+            <div class="side-link-icon">
+              <i class="fas fa-graduation-cap"></i>
+            </div>
+            <span>master programs</span>
+          </a>
+          <ul class="top-nav-secondary">
+            <li>
+              <a target="blank" href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/macro_fundamentals/">Macro Master</a>
+            </li>
+            <li>
+              <a target="blank" href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/Technical_Masters/">Technical Master</a>
+            </li>
+            <li>
+              <a target="blank" href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/learntotradeglobal.options_week/">Options Master</a>
+            </li>
+            <li>
+              <a target="blank" href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/Trading_Master/">Trading Master</a>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a class="top-level-side-nav" href="#resources">
+            <div class="side-link-icon">
+              <i class="fas fa-books"></i>
+            </div>
+            <span>resources</span>
+          </a>
+          <ul class="top-nav-secondary">
+            <li>
+              <a target="_blank" href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/members_home/memberseducation.php">Members Bonus Content</a>
+            </li>
+            <li>
+              <a href="https://www.macrovoices.com/" target="_blank">MacroVoices</a>
+            </li>
+            <li>
+              <a href="https://markethuddle.com/" target="_blank">MarketHuddle</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a class="top-level-side-nav" href="#tools">
+            <div class="side-link-icon">
+              <i class="fas fa-tools"></i>
+            </div>
+            <span>tools</span>
+          </a>
+          <ul class="top-nav-secondary">
+            <li>
+              <a href="https://secure.bigpicturetrading.com/option.simulator/position_simulator.php" target="_blank">Options Trading Simulator</a>
+            </li>
+            <li>
+              <a href="https://bigpicturetrading.com/wp-content/uploads/2020/05/TrackingtheCalendarPutWrite.xlsx" target="_blank">Put Write Calendar Spreadsheet</a>
+            </li>
+            <li>
+              <a href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/portfolio_strategies/calculators/calc1.html" target="_blank">Covered Call Calculator</a>
+            </li>
+            <li>
+              <a href="https://secure.bigpicturetrading.com/membership/bigpictureanalytics/portfolio_strategies/calculators/calc2.html" target="_blank">Naked Put Calculator</a>
+            </li>
+
+          </ul>
+        </li>
+        <li class="top-menu-account-li">
+          <a class="top-level-side-nav top-menu-account-link" target="_blank" href="http://secure.bigpicturetrading.com/membership/profile">
+            <div class="side-link-icon">
+              <i class="fas fa-user"></i>
+            </div>
+            <span><i class="fas fa-user"></i></span>
+          </a>
+          <ul class="top-nav-secondary">
+            <li>
+              <a href="http://secure.bigpicturetrading.com/membership/profile">Edit Profile</a>
+            </li>
+            <li>
+              <a href="http://secure.bigpicturetrading.com/membership/logout">Log Out</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+
+    </div>
+  </nav>
+</header>
